6224214d6a912ba35fab2c03eff22656baaf2b4a
devD <cloudsperk3000@gmail.com>
2026-07-05 11:14:42 +0530

    first node added

--- project.py
+++ project.py
@@ -6,3 +6,24 @@ class pipelinestate(TypedDict):
     edited_text: str
     script_text: str
     final_output: str
+
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(model="llama3.3-70b-versatile", temperature=0.7)
+
+def editor_node(state: pipelinestate) -> dict:
+    """Stage 1 - Cleans up grammer, removes typos and refine the tone"""
+
+    prompt = (
+        "you are an expert copyeditor, clean up the following raw text"
+        "fix any gramatical error, spelling mistake and smooth ouut the transition flow"
+        "while keeping the core message intact, return only the edited text.\n\n"
+        f"Text:\n{state['raw_input']}"
+    )
+
+    response = llm.invoke(prompt)
+
+    return {"edited_text": response.content.strip()}
